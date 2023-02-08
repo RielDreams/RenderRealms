ace84bef3f88373f6f08944c9d4e976392381991
Ariel <117614710+RielDreams@users.noreply.github.com>
2023-02-08 10:35:29 -0500

    fixing up assets page

--- fenderblender/settings.py
+++ fenderblender/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = "django-insecure-t$o3yu#=5if9v9#$q18jr+!)q4ynut^wer+8jsz2y5(!zxk-cr"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
